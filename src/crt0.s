6075f1a32a88195bb3106a9f2f058e5146e0af45
David Lopez <espencer@gmail.com>
2024-10-27 22:05:42 +0100

    fmt asm

--- src/crt0.s
+++ src/crt0.s
@@ -5,46 +5,49 @@
 ;; by H. Hansen 2003
 ;; Original lines has been marked out!
 
-  .module crt0
-  .globl  _main
+.module crt0
+.globl  _main
 
-  .area _HEADER (ABS)
+.area _HEADER (ABS)
+
 ;; Reset vector
-  .org  0x100 ;; Start from address &100
-  jp  init
-  
-  .org  0x110
+.org 0x100 // Start from address &100
+jp   init
+
+.org 0x110
 
 init:
 
-;; Stack at the top of memory.
-;;  ld  sp,#0xffff        
-;;  I will use the Basic stack, so the program can return to basic!
+	;; Stack at the top of memory.
+	;;  ld  sp,#0xffff
+	;;  I will use the Basic stack, so the program can return to basic!
 
-;; Initialise global variables
-  call    gsinit
-  call  _main
-  jp  _exit
+	;; Initialise global variables
+	call gsinit
+	call _main
+	jp   _exit
 
-  ;; Ordering of segments for the linker.
-  .area _HOME
-  .area _CODE
-  .area   _GSINIT
-  .area   _GSFINAL
-        
-  .area _DATA
-  .area   _BSS
-  .area   _HEAP
+	;; Ordering of segments for the linker.
+	.area _HOME
+	.area _CODE
+	.area _GSINIT
+	.area _GSFINAL
 
-  .area   _CODE
+	.area _DATA
+	.area _BSS
+	.area _HEAP
+
+	.area _CODE
+
 __clock::
-  ret
-  
-_exit::
-  ret
-  
-  .area   _GSINIT
-gsinit::  
+	ret
 
-.area   _GSFINAL
-  ret
+_exit::
+	ret
+
+.area _GSINIT
+
+gsinit::
+
+	.area _GSFINAL
+	ret
--- src/putchar.s
+++ src/putchar.s
@@ -3,19 +3,20 @@
 ;; by H. Hansen 2003
 ;; Original lines has been marked out!
 
-  .area _CODE
-_putchar::       
-_putchar_rr_s:: 
-          ld      hl,#2
-          add     hl,sp
-        
-          ld      a,(hl)
-          call    0xBB5A
-          ret
-           
+.area _CODE
+
+_putchar::
+_putchar_rr_s::
+	ld  hl, #2
+	add hl, sp
+
+	ld   a, (hl)
+	call 0xBB5A
+	ret
+
 _putchar_rr_dbs::
 
-          ld      a,e
-          call    0xBB5A
-          ret
+	ld   a, e
+	call 0xBB5A
+	ret
 
